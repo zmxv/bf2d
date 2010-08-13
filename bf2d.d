45cf175a602e8f1bb6b2103f666d74da25dca780
Zhen Wang <z@jdkz.local>
2010-08-12 23:20:35 -0700

    first commit

---
+++ bf2d.d
@@ -0,0 +1,20 @@
+
+
+import std.stdio;alias ulong D;
+void main() { static D[] _= [41480,
+   29974,16834,4642,24913,4716,16674,
+   4802,91166299         ,856_81_557*
+   128002048939]           ;int O,l=1;
+   void d(D D){D           =_[D];while
+   (15&D)printf(           ";%.*s"[l=!
+   l..1],`while(`        `*_= ge++BF2D`
+  `voidBY_000]g}         putchar=impor`
+  `t main(){WANGint[30--ZHENstd.stdio`
+[D/16%16*6..D%16+D/16%16*6]),D>>=8;}
+d(9);while(1+(O=getchar))foreach(D
+
+
+D;_[0..4])if(!((l=O-(127&_[8]>>((D>>13
+)%4*7)))&~2))d(2*l--+(D>>13)%4);d(4);}
+
+
